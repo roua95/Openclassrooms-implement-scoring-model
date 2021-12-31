07032214bc76be7d9fc48518f98f2c45513f6ee4
Roua KHENISSI <rouakhenissi22@gmail.com>
2021-12-31 16:13:44 +0100

    update procfie

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import numpy as np
 
+
 class Client(BaseModel):
     AMT_ANNUITY: float
     EXT_SOURCES_MAX: float
+
 
 def my_score(y_pred, y_true):
     tp = np.sum(y_pred * y_true, axis=0)
@@ -39,19 +41,19 @@ def index():
 
 
 @app.get('/client/{id}')
-def get_client_id(id: int):
-    return {'message': f'Hello! @{id}'}
+def get_client_id(idd: int):
+    return {'message': f'Hello! @{idd}'}
 
 
-@app.get('/prediction')
+"""@app.get('/prediction')
 def get_model_decision(data: Client):
     received = data.dict()
     AMT_ANNUITY = received['AMT_ANNUITY']
     EXT_SOURCES_MAX = received['EXT_SOURCES_MAX']
     pred_name = model.predict([[AMT_ANNUITY, EXT_SOURCES_MAX]]).tolist()[0]
-    return {'prediction': pred_name}
+    return {'prediction': pred_name}"""
 
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=80, debug=True)
+    uvicorn.run(app, host='127.0.0.1', port=4000, debug=True)
